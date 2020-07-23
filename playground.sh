a50b00b94bab496bf44c36d8f395b49254545d61
Albert <ananabeka@wpi.edu>
2020-07-23 08:53:04 -0400

    changes

--- playground.sh
+++ playground.sh
@@ -1,2 +1,4 @@
 #God helps me bring the good food on the table
-
+#Feel yourself the best guy everyday
+#Life techs you lesson while i teach myself how to navigate throughout this crisis
+#Today is a new  day, you should be able to allow yourself to be the best thatn ever
